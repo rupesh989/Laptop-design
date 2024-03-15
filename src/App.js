bb4077601bfb58e0ded3c92c1ae24909e0bd04c4
rupesh989 <rupeshbavaskar24@gmail.com>
2024-03-15 17:02:31 +0530

    Laptop design project added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Background from "./components/Background";
+import Laptop from "./components/Laptop";
+
+const App = () => {
+  return (
+    <>
+      <Background />
+      <Laptop />
+    </>
+  );
+};
+
+export default App;
